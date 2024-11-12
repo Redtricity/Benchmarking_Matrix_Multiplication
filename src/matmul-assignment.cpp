876d148c0c7b5b7a63ad66d04929ac610a2f6681
Shnebs <124152154+Shnebs@users.noreply.github.com>
2024-11-12 12:01:21 +0000

    paul stuff

--- src/matmul-assignment.cpp
+++ src/matmul-assignment.cpp
@@ -32,6 +32,10 @@ struct mat
       for (int j = 0; j < sz; j++) {
         const float abs_diff = std::abs(this->data[i*sz+j] - rhs.data[i*sz+j]);
         b_ret = b_ret && (abs_diff < tolerance);
+        /*if (!b_ret)
+        {
+            std::cout << i << ' ' << j << ' ' << abs_diff << std::endl;
+        }*/
       }
     }
 
@@ -81,13 +85,19 @@ void init_mat(mat &m) {
   for (int i = 0; i < m.sz; i++) {
     for (int j = 0; j < m.sz; j++) {
       m.data[i*m.sz+j] = count++;
+      if (count == 10)
+          count = 1;
     }
   }
 }
 
 int main(int argc, char *argv[])
 {
+<<<<<<< Updated upstream
   unsigned int SZ = 1 << 3; // (1 << 10) == 1024
+=======
+  unsigned int SZ = 1 << 10; // (1 << 10) == 1024 (Matrix size is 8 - LM)
+>>>>>>> Stashed changes
   // n.b. these calls to new have no alignment specifications
   mat mres{new float[SZ*SZ],SZ},m{new float[SZ*SZ],SZ},id{new float[SZ*SZ],SZ};
   mat mres_simd{new float[SZ*SZ],SZ};
@@ -114,16 +124,65 @@ int main(int argc, char *argv[])
   d = duration_cast<microseconds>(t2-t1).count();
   std::cout << d << ' ' << "microseconds.\n";
 
+<<<<<<< Updated upstream
   print_mat(m);
   print_mat(m);
   print_mat(mres);
 
   const bool correct = mres_simd==mres;
   //assert(correct); // uncomment when you have implemented matmul_simd
+=======
+  // Timing Parallel Multiplication (Original) - LM
+  int num_threads = 4; // Num of threads - LM
+  t1 = high_resolution_clock::now();
+  matmul_parallel(mres_parallel, m, m, num_threads); // Parallel Multiplication - LM
+  t2 = high_resolution_clock::now();
+
+  d = duration_cast<microseconds>(t2 - t1).count();
+  std::cout << "Parallel multiplication time with " << num_threads << " threads: " << d << ' ' << "microseconds.\n";
+
+  // Timing Parallel Multiplication with simd - LM
+  //int num_threads = 4; // Duplicate - LM
+  t1 = high_resolution_clock::now();
+  matmul_parallel_simd(mres_parallel_simd, m, m, num_threads); // Parallel Multiplication - LM
+  t2 = high_resolution_clock::now();
+
+  d = duration_cast<microseconds>(t2 - t1).count();
+  std::cout << "Parallel simd multiplication time with " << num_threads << " threads: " << d << ' ' << "microseconds.\n";
+
+  //Prints the matrix results - LM
+
+  std::cout << "Initial Matrix:" << std::endl;
+  print_mat(m); // Print simple multiplication
+
+  std::cout << "Simple multiplication:" << std::endl;
+  print_mat(mres);
+
+  std::cout << "Simd:" << std::endl;
+  print_mat(mres_simd); // Print result of simd - LM
+
+  std::cout << "Parallel:" << std::endl;
+  print_mat(mres_parallel); // Print result of parallel multiplication - LM
+
+  std::cout << "Parallel with Simd:" << std::endl;
+  print_mat(mres_parallel_simd); // Print result of parallel with simd multiplication - LM
+
+  // Assert statement to check if both multiplications are the same.
+  bool b1 = mres == mres_parallel;
+  bool b2 = mres == mres_simd;
+  bool b3 = mres == mres_parallel;
+  bool b4 = mres == mres_parallel_simd;
+
+  assert(b1);
+  assert(b2);
+  assert(b3); // ok
+  assert(b4);
+>>>>>>> Stashed changes
 
   delete [] mres.data;
   delete [] mres_simd.data;
   delete [] m.data;
   delete [] id.data;
-  return correct ? 0 : -1;
+
+  return 0;
 }
